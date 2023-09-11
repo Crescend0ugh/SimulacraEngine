4404109e119cdb4b44d2e75a58a5d1b59f8f91dd
Adithya Somashekhar <avsom101@gmail.com>
2023-09-11 01:20:48 -0400

    ray

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "Source/color.h"
+#include "Source/ray.h"
 #include "Source/vec3.h"
+
+color ray_color(const ray& r)
+{
+    return color (0,0,0);
+}
+
+
 int main() {
 
     // Image
 
+    auto aspect_ratio = 16.0/9.0;
+
     int image_width = 256;
-    int image_height = 256;
+    int image_height = static_cast<int>(image_width/aspect_ratio);
+    image_height = (image_height<1) ? 1 : image_height;
+
+    //Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    auto camera_center = point3(0,0,0);
 
     // Render
 
